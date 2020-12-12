e132306f3abb5b80931ba6b0e1c6dc920f63c354
Carlos Artunduaga <cartunduaga@gmail.com>
2020-12-11 19:33:14 -0500

    corregir archivo

--- models/user_models.py
+++ models/user_models.py
@@ -3,12 +3,7 @@ from pydantic import BaseModel
 class UserIn(BaseModel):
     username: str
     password: str
-    nombres: str
-    apellidos: str
-    direccion: str
-    telefono: str
-    email: str
-    nacionalidad: str
+   
 
 class UserOut(BaseModel):
     username: str
